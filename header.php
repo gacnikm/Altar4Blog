57889ded6b9464835a758d612d3c343bd410dc51
Matej <paxmanpwnz@gmail.com>
2019-04-26 11:27:23 +0200

    Change logo and site description style

--- header.php
+++ header.php
@@ -13,59 +13,77 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			if( has_custom_logo() ){
-			the_custom_logo();
-		}else{
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$altar_description = get_bloginfo( 'description', 'display' );
-			if ( $altar_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $altar_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; 
-		}?>
-		</div><!-- .site-branding -->
+    <header id="masthead" class="site-header">
+        <div class="site-branding">
 
-		<div class="menu-button">
-			<button class="hamburger hamburger--collapse" type="button">
+
+            <?php
+            if (has_custom_logo()) {
+                the_custom_logo();
+            }
+            if (is_front_page() && is_home()) :
+
+            ?>
+            <h1 class="site-title">
+                <?php
+                else:
+                ?>
+                <p class="site-title">
+                    <?php
+                    endif;
+                    ?>
+                    <a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a>
+                <?php
+
+                if (is_front_page() && is_home()) :
+
+                ?></h1>
+        <?php
+        else:
+            ?>
+            </p>
+        <?php
+        endif;
+        $altar_description = get_bloginfo('description', 'display');
+        if ($altar_description || is_customize_preview()) :
+        ?>
+            <p class="site-description"><?php echo $altar_description; /* WPCS: xss ok. */ ?></p>
+            <?php endif;
+
+            ?></h1>
+
+        </div><!-- .site-branding -->
+
+        <div class="menu-button">
+            <button class="hamburger hamburger--collapse" type="button">
 			  <span class="hamburger-box">
 			    <span class="hamburger-inner"></span>
 			  </span>
-			</button>
-		</div>
+            </button>
+        </div>
 
-		<nav id="site-navigation" class="main-navigation">
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'primary-menu',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
+        <nav id="site-navigation" class="main-navigation">
+            <?php
+            wp_nav_menu(array(
+                'theme_location' => 'primary-menu',
+                'menu_id' => 'primary-menu',
+            ));
+            ?>
+        </nav><!-- #site-navigation -->
 
-		<div class="site-search">
-			<div class="search-button"><span class="lnr lnr-magnifier"></span></div>
-			<?php get_search_form(); ?>
-		</div>
-	</header><!-- #masthead -->
+        <div class="site-search">
+            <div class="search-button"><span class="lnr lnr-magnifier"></span></div>
+            <?php get_search_form(); ?>
+        </div>
+    </header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+    <div id="content" class="site-content">
